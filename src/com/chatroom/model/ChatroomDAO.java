83e0b5839c383a2f3adf950c1ea256223aadb0ce
averysun <digitalis14@gmail.com>
2021-12-09 10:32:54 +0800

    1209

---
+++ src/com/chatroom/model/ChatroomDAO.java
@@ -0,0 +1,274 @@
+package com.chatroom.model;
+
+import java.util.*;
+import java.sql.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ChatroomDAO implements ChatroomDAOIpml {
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/root");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+		private static final String INSERT_STMT = 
+			"INSERT INTO chat_room (chat_room_No,rent_id,lease_id,chat_No,chat_text,chat_time) VALUES (?, ?, ?, ?, ?, ?)";
+		private static final String GET_ALL_STMT = 
+			"SELECT chat_room_No,rent_id,lease_id,chat_No,chat_text,chat_time FROM chat_room order by chat_room_No";
+		private static final String GET_ONE_STMT = 
+			"SELECT chat_room_No,rent_id,lease_id,chat_No,chat_text,chat_time FROM chat_room where chat_room_No = ?";
+		private static final String DELETE = 
+			"DELETE FROM chat_room where chat_room_No = ?";
+		private static final String UPDATE = 
+			"UPDATE chat_room set chat_room_No=?, rent_id=?, lease_id=?, chat_No=?, chat_text=?, chat_time=? where chat_room_No = ?";
+
+		
+		public void insert(ChatroomVO chatroomVO) {
+
+			Connection con = null;
+			PreparedStatement pstmt = null;
+
+			try {
+
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(INSERT_STMT);
+
+				pstmt.setInt(1, chatroomVO.getChatNo());
+				pstmt.setInt(2, chatroomVO.getRentId());
+				pstmt.setInt(3, chatroomVO.getLeaseId());
+				pstmt.setInt(4, chatroomVO.getChatNo());
+				pstmt.setString(5, chatroomVO.getChatText());
+				pstmt.setTimestamp(6, chatroomVO.getChatTime());
+
+				pstmt.executeUpdate();
+
+				// Handle any SQL errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+
+		}
+
+		@Override
+		public void update(ChatroomVO chatroomVO) {
+
+			Connection con = null;
+			PreparedStatement pstmt = null;
+
+			try {
+
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(UPDATE);
+
+				pstmt.setInt(1, chatroomVO.getChatroomNo());
+				pstmt.setInt(2, chatroomVO.getRentId());
+				pstmt.setInt(3, chatroomVO.getLeaseId());
+				pstmt.setInt(4, chatroomVO.getChatNo());
+				pstmt.setString(5, chatroomVO.getChatText());
+				pstmt.setTimestamp(6, chatroomVO.getChatTime());
+
+				pstmt.executeUpdate();
+
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+
+		}
+
+		@Override
+		public void delete(Integer chatRoomNo) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+
+			try {
+
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(DELETE);
+
+				pstmt.setInt(1, chatRoomNo);
+
+				pstmt.executeUpdate();
+
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+
+			
+		}
+
+		@Override
+		public ChatroomVO findByPrimaryKey(Integer leaseId) {
+			ChatroomVO chatroomVO = null;
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(GET_ONE_STMT);
+
+				pstmt.setInt(1, leaseId);
+
+				rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+					chatroomVO = new ChatroomVO();
+					chatroomVO.setChatroomNo(rs.getInt("chat_room_No"));
+					chatroomVO.setRentId(rs.getInt("rent_id"));
+					chatroomVO.setLeaseId(rs.getInt("lease_id"));
+					chatroomVO.setChatNo(rs.getInt("chat_No"));
+					chatroomVO.setChatText(rs.getString("chat_text"));
+					chatroomVO.setChatTime(rs.getTimestamp("Chat_time"));
+				}
+
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+			return chatroomVO;
+		}
+
+		@Override
+		public List<ChatroomVO> getAll() {
+			List<ChatroomVO> list = new ArrayList<ChatroomVO>();
+			ChatroomVO chatroomVO = null;
+
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+
+			try {
+
+				con = ds.getConnection();
+				pstmt = con.prepareStatement(GET_ALL_STMT);
+				rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+					chatroomVO = new ChatroomVO();
+					chatroomVO.setChatroomNo(rs.getInt("chat_room_No"));
+					chatroomVO.setRentId(rs.getInt("rent_id"));
+					chatroomVO.setLeaseId(rs.getInt("lease_id"));
+					chatroomVO.setChatNo(rs.getInt("chat_No"));
+					chatroomVO.setChatText(rs.getString("chat_text"));
+					chatroomVO.setChatTime(rs.getTimestamp("chat_time"));
+					list.add(chatroomVO); 
+				}
+
+				// Handle any driver errors
+			} catch (SQLException se) {
+				throw new RuntimeException("A database error occured. "
+						+ se.getMessage());
+				// Clean up JDBC resources
+			} finally {
+				if (rs != null) {
+					try {
+						rs.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (SQLException se) {
+						se.printStackTrace(System.err);
+					}
+				}
+				if (con != null) {
+					try {
+						con.close();
+					} catch (Exception e) {
+						e.printStackTrace(System.err);
+					}
+				}
+			}
+			return list;
+		}
+		
+		
+}
---
+++ src/com/pbreply/model/pbReplyJDBCDAO.java
@@ -0,0 +1,353 @@
+package com.pbreply.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.*;
+
+
+public class pbReplyJDBCDAO implements pbReplyDAOIpml {
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/JoyLease?serverTimezone=Asia/Taipei";
+	String userid = "root";
+	String passwd = "password";
+
+	private static final String INSERT_STMT = 
+		"INSERT INTO pb_reply (reply_id,post_id,member_id,reply_cont,reply_time) VALUES (?, ?, ?, ?, ?)";
+	private static final String GET_ALL_STMT = 
+		"SELECT reply_id,post_id,member_id,reply_cont,reply_time FROM pb_reply order by reply_id";
+	private static final String GET_ONE_STMT = 
+		"SELECT reply_id,post_id,member_id,reply_cont,reply_time FROM pb_reply where reply_id = ?";
+	private static final String DELETE = 
+		"DELETE FROM pb_reply where reply_id = ?";
+	private static final String UPDATE = 
+		"UPDATE pb_reply set post_id=?, member_id=?, reply_cont=? reply_time=?  where reply_id = ?";
+	
+	
+	
+	@Override
+	public void insert(pbReplyVO pbreplyVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+			pstmt.setInt(1, pbreplyVO.getReplyId());
+			pstmt.setInt(2, pbreplyVO.getPostId());
+			pstmt.setInt(3, pbreplyVO.getMemberId());
+			pstmt.setString(4, pbreplyVO.getReplyCont());
+			pstmt.setTimestamp(5, pbreplyVO.getReplyTime());
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+		
+	}
+	@Override
+	public void update(pbReplyVO pbreplyVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(UPDATE);
+
+			pstmt.setInt(1, pbreplyVO.getReplyId());
+			pstmt.setInt(2, pbreplyVO.getPostId());
+			pstmt.setInt(3, pbreplyVO.getMemberId());
+			pstmt.setString(4, pbreplyVO.getReplyCont());
+			pstmt.setTimestamp(5, pbreplyVO.getReplyTime());
+		
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+		
+	}
+	@Override
+	public void delete(Integer replyId) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(DELETE);
+
+			pstmt.setInt(1, replyId);
+
+			pstmt.executeUpdate();
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		
+	}
+	@Override
+	public pbReplyVO findByPrimaryKey(Integer replyId) {
+		pbReplyVO pbreplyVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+
+			pstmt.setInt(1, replyId);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				
+				pbreplyVO = new pbReplyVO();
+				pbreplyVO.setReplyId(rs.getInt("reply_id"));
+				pbreplyVO.setPostId(rs.getInt("post_id"));
+				pbreplyVO.setMemberId(rs.getInt("member_id"));
+				pbreplyVO.setReplyCont(rs.getString("reply_cont"));
+				pbreplyVO.setReplyTime(rs.getTimestamp("reply_time"));
+				
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return pbreplyVO;
+	}
+	@Override
+	public List<pbReplyVO> getAll() {
+		List<pbReplyVO> list = new ArrayList<pbReplyVO>();
+		pbReplyVO pbreplyVO = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				
+				pbreplyVO = new pbReplyVO();
+				pbreplyVO.setReplyId(rs.getInt("reply_id"));
+				pbreplyVO.setPostId(rs.getInt("post_id"));
+				pbreplyVO.setMemberId(rs.getInt("member_id"));
+				pbreplyVO.setReplyCont(rs.getString("reply_cont"));
+				pbreplyVO.setReplyTime(rs.getTimestamp("reply_time"));
+				list.add(pbreplyVO); // Store the row in the list
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+
+		pbReplyJDBCDAO dao = new pbReplyJDBCDAO();
+		long time = System.currentTimeMillis();
+		Timestamp timestamp = new Timestamp(time);
+
+		// 新增--ok
+		pbReplyVO pbreplyVO1 = new pbReplyVO();
+		pbreplyVO1.setReplyId(5);
+		pbreplyVO1.setPostId(1);
+		pbreplyVO1.setMemberId(5);
+		pbreplyVO1.setReplyCont("test2");
+		pbreplyVO1.setReplyTime(timestamp);
+		dao.insert(pbreplyVO1);
+
+//		// 修改--ok
+//		pbReplyVO pbreplyVO2 = new pb_replyVO();
+//		pbreplyVO2.setReplyId(4);
+//		pbreplyVO2.setPostId(1);
+//		pbreplyVO2.setMemberId(5);
+//		pbreplyVO2.setReplyCont("test2");
+//		pbreplyVO2.setReplyTime(timestamp);
+//		dao.insert(pbreplyVO2);
+	
+//
+//		// 刪除--ok
+//		dao.delete(4);
+//
+//		// 查詢--ok
+//		pbReplyVO pbreplyVO3 = new pbReplyVO();
+//		pbreplyVO3.setReplyId(1);
+//		pbreplyVO3.setPostId(1);
+//		pbreplyVO3.setMemberId(1);
+//		pbreplyVO3.setReplyCont("test");
+//		pbreplyVO3.setReplyTime(timestamp);
+//		dao.findByPrimaryKey(1);
+//		System.out.print(pbreplyVO3.getReplyId() + ",");
+//		System.out.print(pbreplyVO3.getPostId() + ",");
+//		System.out.print(pbreplyVO3.getMemberId() + ",");
+//		System.out.print(pbreplyVO3.getReplyCont() + ",");
+//		System.out.print(pbreplyVO3.getReplyTime() + ",");
+
+//
+//		// 查詢--ok
+		List<pbReplyVO> list = dao.getAll();
+		for (pbReplyVO pbReply : list) {
+			System.out.print(pbReply.getReplyId() + ",");
+			System.out.print(pbReply.getPostId() + ",");
+			System.out.print(pbReply.getMemberId() + ",");
+			System.out.print(pbReply.getReplyCont() + ",");
+			System.out.print(pbReply.getReplyTime() + ",");
+			System.out.println();
+		}
+	}
+	
+	
+	
+	
+	
+
+
+}
