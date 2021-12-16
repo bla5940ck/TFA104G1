40ec7cc32c42d2e73d34a4e691540ea84ff73330
leoshin89 <a1276861940809@gmail.com>
2021-12-16 20:54:56 +0800

    no message

--- src/com/promo_list/controller/getCouponServlet.java
+++ src/com/promo_list/controller/getCouponServlet.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
