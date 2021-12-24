40f1bf6adbb6a7d7aebcf611db6ea02fb7af46cb
ian <ecounterpian@gmail.com>
2021-12-24 16:36:44 +0800

    no message

--- src/com/member/model/MemberDAO.java
+++ src/com/member/model/MemberDAO.java
@@ -20,7 +20,7 @@ public class MemberDAO implements MemberDAO_interface {
 	static {
 		try {
 			Context ctx = new InitialContext();
-			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/Member");
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/JoyLease");
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
